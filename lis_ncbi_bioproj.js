52a2b58fe9c1825aeaed481c68ae654563530298
sdash <sdash@ncgr.org>
2016-06-02 17:09:55 -0500

    Retrievs and shows all project types

--- lis_ncbi_bioproj.js
+++ lis_ncbi_bioproj.js
@@ -40,14 +40,14 @@ function makeHtmlFromBioprojEsummaryJson(esummaryJson) {  //NOT DONE YET winProg
         
         var taxid = esummaryResult[uid]['taxid']; //console.log(taxid);
         var project_id = esummaryResult[uid]['project_id']; //console.log(project_id);
-        var project_acc = esummaryResult[uid]['project_acc']; console.log(project_acc);
+        var project_acc = esummaryResult[uid]['project_acc']; //console.log(project_acc);
         var project_data_type = esummaryResult[uid]['project_data_type']; //console.log(project_data_type);
         var project_target_material = esummaryResult[uid]['project_target_material']; //console.log(project_target_material);
-        var project_name = esummaryResult[uid]['project_name']; console.log(project_name);
+        var project_name = esummaryResult[uid]['project_name']; //console.log(project_name);
         var project_title = esummaryResult[uid]['project_title']; //console.log(project_title);
         var project_description = esummaryResult[uid]['project_description']; //console.log(project_description);
         var organism_name = esummaryResult[uid]['organism_name']; //console.log(organism_name);
-        var project_methodtype = esummaryResult[uid]['project_methodtype']; console.log("project_methodtype" + project_methodtype);
+        var project_methodtype = esummaryResult[uid]['project_methodtype']; //console.log("project_methodtype" + project_methodtype);
         var project_target_material = esummaryResult[uid]['project_target_material']; //console.log(project_target_material);
         
         
@@ -89,7 +89,7 @@ function makeHtmlFromBioprojEsummaryJson(esummaryJson) {  //NOT DONE YET winProg
         var bioproj = "<b>"
             + bioproj_ncbi_link + ": " + "</b>"
             + project_title
-            + " (" + project_methodtype + ")";
+            + " <b>(</b>" + project_data_type + "; "+project_methodtype + "<b>)</b>";
         
         //var more_details = "<a id='collapseLink'  onclick=\"(function($) { $(document).ready(function(){$(this).next('fieldset').toggle('5000'); $('#collapseLink').text(($('#collapseLink').text() == '...less') ? '>[...more]' : '...less');}); })(jQuery);\" "
         //    + ">[...more]</a>" 
@@ -102,13 +102,13 @@ function makeHtmlFromBioprojEsummaryJson(esummaryJson) {  //NOT DONE YET winProg
         
         var more_details = "<a onclick=\"jQuery(this).next('fieldset').toggle();\">&nbsp;&nbsp;Details <b>[&plusmn;]</b></a>"
             + "<fieldset id='details'  style='display:none;background-color: #EFEFEF'>"
-            + "<b>Project name:</b> " + project_name + "&nbsp;"
+            + "<b>Project name:</b> " + project_name + "<br/>"
             + " (" + "<b>Organism:</b> " + "<i>" + organism_name + "</i>" + "; "
             + "<b>Project type:</b> " + project_data_type + "; "
             + "<b>Method:</b> " + project_methodtype + "; "
             + "<b>Target material:</b> " + project_target_material
             + ")." + "<br/>"
-            + "<b>Project description:</b> " + project_description
+            + "<b>Project description:</b><br/> " + project_description
             + "</fieldset>";
         //console.log("more_details:"+more_details);
   
@@ -133,7 +133,9 @@ function makeHtmlFromBioprojEsummaryJson(esummaryJson) {  //NOT DONE YET winProg
 
 
 
-function FillDomElementWithBioprojHtml (genus, method, domElementId) {
+function FillDomElementWithBioprojHtml (genus, projectDataType, method, domElementId) {
+    // Formatting in the console for easy visibility
+    console.log('\n' + '**** ' + new Date().toLocaleString() + ' ****'); 
     
     var message = "";
     var messageInitial = "<span style='font-size:1.5em;color:#999999'>Please wait: Gettting data from Pubmed ...   ...   ...</span>";
@@ -141,9 +143,11 @@ function FillDomElementWithBioprojHtml (genus, method, domElementId) {
     jQuery("#" + domElementId).html (messageInitial);
     
     //Get the selected genus and method from form
-    genus = jQuery("form#genus  option:selected").val()
-    method = jQuery("form#method  input:checked").val()   
-    
+    projectDataType = jQuery("form#projectDataType  input:checked").val();
+    genus = jQuery("form#genus  option:selected").val();
+    method = jQuery("form#method  input:checked").val();
+       
+    console.log("projectDataType: " + projectDataType); //debug
     console.log("genus: " + genus); //debug
     console.log("method: " + method); //debug
      
@@ -160,18 +164,48 @@ function FillDomElementWithBioprojHtml (genus, method, domElementId) {
     
     var termOrganism = "(" + genus + "[Organism])";
     var termMethod = "(" + "\"method " +  method +"\"[Properties])";
+    var termProjectDataType = '';
     
+    switch (projectDataType) {
+        case 'All':
+            termProjectDataType = '';
+            break;
+        case 'GeneExp':
+            termProjectDataType = "+AND+(\"Transcriptome+or+Gene+expression\"[Project+Data+Type])";
+            break;
+        case 'Variation':
+            termProjectDataType = "+AND+(\"variation\"[Project+Data+Type])";
+            break;
+    }
+    
+    
+    //>>>>>>>>>>>>>>>>>>>>>>
+    //for projectDataType:
+    //original//var query = "";
+    
+    query = "&term=" + termOrganism + termProjectDataType;
+    
+    if (method != "All") {
+        query = query + "+AND+" + termMethod;
+    } else {
+        query = query;
+        
+    }
+    
+    //Original<<<<<
     //for method:
-    var query = "";
+    /*
     if (method != "All") {
         query = "&term=" + termOrganism + "+AND+" + "(\"Transcriptome+or+Gene+expression\"[Project+Data+Type])"
                + "+AND+" + termMethod;
     } else {
         query = "&term=" + termOrganism + "+AND+" + "(\"Transcriptome+or+Gene+expression\"[Project+Data+Type])";
-    }
+    }*/
+    //>>>>>>>>>>>>>
     
     var UrlEsearch = BaseUrlEsearch + query; //returns json obj
-    console.log("UrlEsearch: " + UrlEsearch); 
+    
+    //console.log("UrlEsearch: " + UrlEsearch); 
     
     //
     
@@ -182,21 +216,21 @@ function FillDomElementWithBioprojHtml (genus, method, domElementId) {
         var esearchCount = esearchJson.esearchresult.count;
         
         //Message while waiting 'No of items found'
-        message = "<b>" + "Found " + esearchCount + " projects at NCBI " + "for " + genus + " with " + "method, " + "'" + method + "'" + ". " + "</b>" + "<br/><br/>";
+        message = "<i>" + "Found " + "<b>" + esearchCount + " projects" + "</b>" + " at NCBI " + "for " + genus + "</i><br/><br/>";// + " and " + projectDataType + " data type " + " with " + "method, " + "'" + method + "'" + ". " + "</b>" + "<br/><br/>";
         //jQuery("#" + domElementId).html (message);
         var esearchRetmax = esearchJson.esearchresult.retmax;
         console.log ("esearchCount: " + esearchCount + "; esearchRetmax: " + esearchRetmax); //debug
         
         //bioproj id list from Esearch
         var esearchIdlist = esearchJson.esearchresult.idlist;
-        console.log ("esearchIdlist: " + esearchIdlist.join() ); //debug
+        //console.log ("esearchIdlist: " + esearchIdlist.join() ); //debug
         
         jQuery("#" + domElementId).html (message);
         //jQuery("#" + domElementId).html ("esearchIdlist: " + esearchIdlist.join());
         
         var esummaryUrl = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=bioproject&retmode=json" + "&id="
             + esearchIdlist.join();
-        console.log("esummaryUrl: " + esummaryUrl); //debug
+        //console.log("esummaryUrl: " + esummaryUrl); //debug
         
         jQuery.get(esummaryUrl,status, function(esummaryJson){
             
--- lis_ncbi_bioproj_page.tpl.php
+++ lis_ncbi_bioproj_page.tpl.php
@@ -1,20 +1,21 @@
 <!-- Title and guide text  -->
 <div>
   <h1>NCBI BioProjects (legumes)</h1>
-  <span style="font-size: 85%">&nbsp;(List of Transcriptome/ Gene Expression related 'BioProjects' at NCBI for LIS legume species)</span>
+  <span style="font-size: 85%">&nbsp;(List of 'BioProjects' at NCBI for LIS legume species. Includes 'Transcriptome/ Gene Expression' related and 'Variation' related data types among others.)</span>
 </div>
 <script  type="text/javascript"  src="/sites/all/modules/lis_ncbi_bioproj/lis_ncbi_bioproj.js"></script>
 <script>
   var genus = "<?php echo $genus ?>";
   var method = "<?php echo $method ?>";
+  var projectDataType = "<?php echo $project_data_type ?>";
 </script>
 <!-- -----------------------------------------------------------------------  -->
 
 <div>
-  <fieldset>
+  <fieldset  style="display: inline-block; margin-top: 1px; margin-bottom: 10px">
     <form id="genus" action="">
       <b>&nbsp;Genus:&nbsp;&nbsp;</b>  
-      <select  id="genus" onchange="FillDomElementWithBioprojHtml (this.options[this.selectedIndex].value, method, 'bioprojectList'); console.log('selected-option-value: ' + this.options[this.selectedIndex].value);">
+      <select  id="genus" onchange="FillDomElementWithBioprojHtml (this.options[this.selectedIndex].value, projectDataType, method, 'bioprojectList'); console.log('selected-option-value: ' + this.options[this.selectedIndex].value);">
           <option value="Fabaceae">  All Legumes (Fabaceae) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</option>
           <option value="Apios">  Apios  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</option>
           <option value="Arachis">  Arachis  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</option>
@@ -36,37 +37,32 @@
       </select>
     </form>
     
-    <form id="method"  action="">
-            <b>&nbsp;Method:</b>&nbsp;&nbsp;
-            <input type="radio" name="method" value="All"  checked="checked"  onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;All &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="method" value="Array"    onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;Array&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="method" value="Sequencing"  onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;Sequencing
-    </form>  
+    <form id="projectDataType"  action="">
+      <b>&nbsp;Data Type:</b>&nbsp;&nbsp;
+      <input type="radio" name="projectdatatype" value="All"  onclick="FillDomElementWithBioprojHtml (genus, this.value, method, 'bioprojectList');">&nbsp;&nbsp;All &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="radio" name="projectdatatype" value='GeneExp'  checked="checked"  onclick="FillDomElementWithBioprojHtml (genus, this.value, method, 'bioprojectList');">&nbsp;&nbsp;Transcriptome/Gene Expression&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="radio" name="projectdatatype" value='Variation'    onclick="FillDomElementWithBioprojHtml (genus, this.value, method, 'bioprojectList');">&nbsp;&nbsp;Variation&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </form>
+    
+    <fieldset style="margin-left: 100px;margin-top: 5px;margin-bottom: 5px;margin-right: 100px">
+      <form id="method"  action="">
+              <b>&nbsp;Method:</b>&nbsp;&nbsp;
+              <input type="radio" name="method" value="All"  onclick="FillDomElementWithBioprojHtml (genus, projectDataType, this.value, 'bioprojectList');">&nbsp;&nbsp;All &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <input type="radio" name="method" value="Array"  onclick="FillDomElementWithBioprojHtml (genus, projectDataType, this.value, 'bioprojectList');">&nbsp;&nbsp;Array&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <input type="radio" name="method" value="Sequencing"  checked="checked"  onclick="FillDomElementWithBioprojHtml (genus, projectDataType, this.value, 'bioprojectList');">&nbsp;&nbsp;Sequencing&nbsp;&nbsp;
+      </form>
+    </fieldset>
   </fieldset>
 </div>
     
-<!--
-    <form id="period"  action="">
-            <!--<legend>Period:</legend><br> - ->
-            <b>Period (Reconsider if period is relevant or even possible; not months but years):</b>&nbsp;&nbsp;
-            <input type="radio" name="period" value="1"  onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;Last month &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="period" value="3"  checked="checked"  onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;Last 3 months&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="period" value="6"  onclick="FillDomElementWithBioprojHtml (genus, this.value, 'bioprojectList');">&nbsp;&nbsp;Last 6 months
-    </form>  
--->
-
-
-
 
 
 <script>
   //For initial page loading before user interaction
-FillDomElementWithBioprojHtml (genus, method, 'bioprojectList');
+FillDomElementWithBioprojHtml (genus, projectDataType, method, 'bioprojectList');
 </script> 
 <!--  ---------------------------------------------------------------------   -->
 <hr/>
-<!--<iframe id="frameviewer" frameborder="0" width="1200" height="2500" scrolling="yes"  src="http://www.ncbi.nlm.nih.gov/pubmed?linkname=pubmed_pubmed_citedin&from_uid=15608283"  name="frameviewer">-->
-</div>
 
 <div id="bioprojectList">
   
